90e1cfb26d2ef76fcef9708d02bd109ffd41d409
roman <rhromets@gmail.com>
2021-07-28 21:49:50 +0300

    initial bmi-calculator project

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BMICalculator());
+}
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Color(0xFF0A0E21),
+      ),
+      home: InputPage(
+        title: "BMI Calculator",
+      ),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  InputPage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Body text',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
